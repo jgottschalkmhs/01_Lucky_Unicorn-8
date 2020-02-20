ee6193bc310dd9bd0468e538a4c0510fc0f68177
tee71639 <58056940+tee71639@users.noreply.github.com>
2020-02-21 10:49:59 +1300

    ,

---
+++ 04_Payment_Mechanics.py
@@ -0,0 +1,32 @@
+# Lucky Unicorn - payment mechanics
+
+# To do
+# Assume starting amount is $10
+# Allow manual token input for testing purposes
+# Adjust total correctly for a given token
+#   - if it's a unicorn, add $5
+#   - if it's a zebra / horse, subtract 0.5
+# Give user feedback based on winnings...
+# State new total
+
+# Assume starting amount is $10
+total = 10
+
+# Allow manual token input for testing purposes
+token = input("enter a token: ")
+
+# Adjust total correctly for a given token
+if token == "unicorn":
+    total += 5
+    feedback = "Congratulations, you won $5.00!"
+elif token == "donkey":
+    total -= 1
+    feedback = "Sorry, you did not win anything this round."
+else:
+    total -= 0.5
+    feedback = "Sorry, you lost 50c."
+
+print()
+
+print(feedback)
+print("You have {} to play with".format(total))
